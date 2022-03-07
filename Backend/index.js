db4514da9fd2511df865d08c0580c22f83991c45
jorgevarela98 <jormivaroli98@hotmail.com>
2022-03-06 18:14:44 -0600

    feat: agregado de cors y body-parser

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,18 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+
+/**
+ * Middlewares: funciones que tienen acceso a los objetos:
+ * -request (peticion)
+ * -response (respuesta)
+ */
+
+app.use(cors);
+app.use(bodyParser.json());
+
 
 
 
@@ -14,8 +27,3 @@ app.get('/',(req,res)=>{
 app.listen(puerto,()=>{
     console.log( `Servidor Levantado con el puerto ${puerto}`)
 });
-
-
-module.exports= {
-    app:app
-}
