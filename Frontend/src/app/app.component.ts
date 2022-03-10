349359161a2eb35cf95b3c048555b3bf842dacf8
Darwin529 <darwin.rodas@unah.hn>
2022-03-10 16:27:48 -0600

    añadido de carrusel y nodes necesarios

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'honduzon';
 
-  constructor(){}
+  constructor() {}
 
+  ngOnInit() {}
+
+  url: String = '../assets/01.jpg';
+  imageChange(event: any) {
+    this.url = event.target.src;
+  }
+
+  customOptions: OwlOptions = {
+    loop: false,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: true,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 2,
+      },
+      740: {
+        items: 3,
+      },
+      940: {
+        items: 4,
+      },
+    },
+    nav: true,
+  };
 }
