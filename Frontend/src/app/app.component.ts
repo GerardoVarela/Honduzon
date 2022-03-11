c3471812f8457a1c2f0a2b51e8c412974c02ddf1
Darwin529 <darwin.rodas@unah.hn>
2022-03-11 00:24:20 -0600

    Se agregaron los botones del carrusel y refrescar categoria; entre otros detalles pequenios

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -25,7 +25,10 @@ export class AppComponent {
     pullDrag: true,
     dots: true,
     navSpeed: 700,
-    navText: ['', ''],
+    navText: [
+      '<i class="fa fa-caret-left"></i>',
+      '<i class="fa fa-caret-right"></i>',
+    ],
     responsive: {
       0: {
         items: 1,
