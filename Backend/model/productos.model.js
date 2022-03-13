bd6461c49255c6bd7fccc8a1048cfc55ebe4d876
jorgevarela98 <jormivaroli98@hotmail.com>
2022-03-13 16:49:44 -0600

    agregado de productos model

--- Backend/model/productos.model.js
+++ Backend/model/productos.model.js
@@ -12,15 +12,15 @@ async function inserProducto(producto){
     try {
         var pool = await mssql.connect(bdConfig.config);
         let insertarProducto = await pool.request()
-        .input('NOMBRE_PRODUCTO',mssql.VarChar,)
-        .input('DESCRIPCION_PRODUCTO',mssql.VarChar,)
-        .input('CANTIDAD_PRODUCTO',mssql.Int,)
-        .input('CANTIDAD_PROD_VENDIDO',mssql.Int,)
-        .input('VALORACIONES',mssql.Int,)
-        .input('PRECIO',mssql.Float,)
-        .input('ID_USUARIO',mssql.Int,)
-        .input('ID_CATEGORIA',mssql.Int,)
-        .input('IMAGEN',mssql.Image,)
+        .input('NOMBRE_PRODUCTO',mssql.VarChar,producto.formProdName)
+        .input('DESCRIPCION_PRODUCTO',mssql.VarChar,producto.formDescripcion)
+        .input('CANTIDAD_PRODUCTO',mssql.Int,producto.formQuantityProd)
+        .input('CANTIDAD_PROD_VENDIDO',mssql.Int,producto.formQuantitySold)
+        .input('VALORACIONES',mssql.Int,producto.formRanks)
+        .input('PRECIO',mssql.Float,producto.formPrice)
+        .input('ID_USUARIO',mssql.Int,producto.userID)
+        .input('ID_CATEGORIA',mssql.Int,producto.categoryID)
+        .input('IMAGEN',mssql.Image,producto.formImage)
 
 
     } catch (error) {
