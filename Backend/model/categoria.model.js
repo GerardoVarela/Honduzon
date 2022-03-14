8dec68d2c4ec0db944af01109399e36f52ba7c75
jorgevarela98 <jormivaroli98@hotmail.com>
2022-03-13 20:44:13 -0600

    agregado de categorias model

---
+++ Backend/model/categoria.model.js
@@ -0,0 +1,9 @@
+/**
+ * @author: Jvarela
+ * 
+ * Archivo que contendra el modelo para las queries hacia y desde las base de datos para los Productos.
+ * 
+ */
+
+ var bdConfig = require('../config/bd-config');
+ const mssql = require('mssql');
