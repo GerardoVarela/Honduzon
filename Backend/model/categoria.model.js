617b5a2380bf88c059fca6326fdeb0dc6d987cc1
jorgevarela98 <jormivaroli98@hotmail.com>
2022-03-14 00:22:05 -0600

    agregado de cat model

--- Backend/model/categoria.model.js
+++ Backend/model/categoria.model.js
@@ -14,17 +14,10 @@ async function insertCategoria(categoria){
     try {
         var pool = await mssql.connect(bdConfig.config);
         let insertarCategoria = await pool.request()
-        .input('NOMBRE', mssql.VarChar,usuario.formName)
-        .input('APELLIDO', mssql.VarChar,usuario.formLastName)
-        .input('CORREO_ELECTRONICO',mssql.VarChar,usuario.formEmail)
-        .input('TELEFONO',mssql.VarChar,usuario.formPhone)
-        .input('DIRECCION',mssql.VarChar,usuario.formDirection)
-        .input('RESPUESTA',mssql.VarChar,usuario.formResp)
-        .input('ID_DEPARTAMENTO',mssql.Int,usuario.formDept)
-        .input('CONTRASENA',mssql.VarChar,usuario.formPassword)
-        .input('ID_CIUDAD',mssql.Int,usuario.formCity)
-        .input('ID_PREGUNTA',mssql.Int,usuario.formPreg)
-        .execute('SP_INSERTAR_USUARIO'); //NO SE HA CREADO EL STORED PROCEDURE
+        .input('NOMBRE_CATEGORIA', mssql.VarChar,usuario.formCategoryName)
+        .input('DESCRIPCION_CATEGORIA', mssql.VarChar,usuario.formCategoryDescription)
+        .input('IMAGEN',mssql.Image,usuario.formImageInput)
+        .execute('SP_INSERTAR_CATEGORIA'); //NO SE HA CREADO EL STORED PROCEDURE
         return insertarCategoria.recordsets;
     } catch (error) {
         console.log(error);
@@ -42,4 +35,11 @@ async function obtenerCategoria(categoriaId){
 
 async function editarCategoria(categoriaId){
 
+}
+
+module.exports={
+    insertCategoria:insertCategoria,
+    obtenerCategorias:obtenerCategorias,
+    obtenerCategoria:obtenerCategoria,
+    editarCategoria:editarCategoria
 }
