ac4506f47d43c906b6d076ed14c0cf644aaf135d
jorgevarela98 <jormivaroli98@hotmail.com>
2022-03-14 09:19:01 -0600

    agregado de routers: categorias y productos

---
+++ Backend/routes/categorias-routers.js
@@ -0,0 +1,33 @@
+/**
+ *  @author: JVarela
+ * 
+ * Archivo para poner todas las web services de las categorias.
+ * Aca se pondran todos
+ * 
+ * 
+ * @param res= parametro respuesta del servidor hacia el cliente
+ * @param req= parametro request o peticion del cliente al servidor.
+ * 
+ */
+
+var express = require('express');
+var router = express.Router();
+
+var categoriaModel = require ('../model/categoria.model');
+
+
+router.get('/',(req,res)=>{
+    categoriaModel.obtenerCategorias().then(resultado=>{
+        res.send(resultado);
+    });
+});
+
+router.post('/guardar',(req,res)=>{
+    categoriaModel.insertCategoria(req.body).then(resultado=>{
+        res.send({
+            mensaje: 'Categoria ingresada con exito',
+            registroCat: true
+        })
+        res.statusCode(201);
+    })
+});
