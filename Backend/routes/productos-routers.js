d4a38bcd818939e86e61bbba5d501195cdfcb5a5
jorgevarela98 <jormivaroli98@hotmail.com>
2022-03-15 00:55:49 -0600

    agregado routers sin terminar en productos

---
+++ Backend/routes/productos-routers.js
@@ -0,0 +1,14 @@
+/**
+ *  @author: JVarela
+ * 
+ * Archivo para poner todas las web services del de los productos.
+ * Aca se pondran todos
+ * 
+ * 
+ * @param res= parametro respuesta del servidor hacia el cliente
+ * @param req= parametro request o peticion del cliente al servidor.
+ * 
+ */
+
+var express = require('express');
+var router = express.Router();
