f585b84b3d4a2ae78396ee8dcfa59002536cbbaa
jorgevarela98 <jormivaroli98@hotmail.com>
2022-03-15 12:32:35 -0600

    producto y categoria routers

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,7 @@ var obtenerDepartamentos = require ('./routes/departamentos-routers');
 var obtenerCiudades = require ('./routes/ciudad-router');
 var obtenerPreguntas = require('./routes/preguntas-router');
 var login = require('./routes/login-routers')
+var categorias = require('./routes/categorias-routers')
 
 /**
  * Middlewares: funciones que tienen acceso a los objetos:
@@ -23,7 +24,7 @@ app.use ('/departamentos',obtenerDepartamentos.router);
 app.use('/ciudades',obtenerCiudades.router);
 app.use('/preguntas',obtenerPreguntas.router);
 app.use('/login',login.router);
-
+app.use('/categorias',categorias.router)
 
 puerto = 8888;
 
--- Backend/routes/categorias-routers.js
+++ Backend/routes/categorias-routers.js
@@ -31,3 +31,8 @@ router.post('/guardar',(req,res)=>{
         res.statusCode(201);
     })
 });
+
+
+module.exports={
+    router:router
+}
