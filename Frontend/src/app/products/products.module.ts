c1f1b5a5b02fcf4b60977546d481f43caf70cf4a
Gerardo Varela <gerardo.barrientos2000@gmail.com>
2022-03-22 19:52:02 -0600

    poblado de BD

--- Frontend/src/app/products/products.module.ts
+++ Frontend/src/app/products/products.module.ts
@@ -6,39 +6,26 @@ import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 
-import { SharedModule } from '../shared/shared.module';
-
 import { CarruselComponent } from './components/carrusel/carrusel.component';
 import { CardCarruselComponent } from './components/card-carrusel/card-carrusel.component';
-<<<<<<< HEAD
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { RestorePageComponent } from './pages/restore-page/restore-page.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule } from '@angular/common/http';
 import { CarruselProductsComponent } from './components/carrusel-products/carrusel-products.component';
-=======
 import { CardProductsComponent } from './components/card-products/card-products.component';
-import { LandingPageComponent } from './pages/landing-page/landing-page.component';
-import { RestorePageComponent } from './pages/restore-page/restore-page.component';
 import { ProductsListPageComponent } from './pages/products-list-page/products-list-page.component';
 import { CardSalesPersonComponent } from './components/card-sales-person/card-sales-person.component';
->>>>>>> 323e4fc41e370b3390badbe8a9adbdd402bdad8c
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     CarruselComponent,
     CardCarruselComponent,
-<<<<<<< HEAD
-    LandingPageComponent,
-    RestorePageComponent,
-    CarruselProductsComponent,
-=======
     CardProductsComponent,
     CardSalesPersonComponent,
     LandingPageComponent,
     RestorePageComponent,
     ProductsListPageComponent,
->>>>>>> 323e4fc41e370b3390badbe8a9adbdd402bdad8c
+    CarruselProductsComponent
   ],
   imports: [
     CommonModule,
@@ -47,14 +34,6 @@ import { CardSalesPersonComponent } from './components/card-sales-person/card-sa
     CarouselModule,
     NgbModule,
     HttpClientModule,
-<<<<<<< HEAD
-    SharedModule,
-  ],
-  exports: [
-    LandingPageComponent,
-    RestorePageComponent,
-    CarruselProductsComponent,
-=======
     SharedModule
   ],
   exports: [
@@ -63,8 +42,8 @@ import { CardSalesPersonComponent } from './components/card-sales-person/card-sa
     CardSalesPersonComponent,
     LandingPageComponent,
     RestorePageComponent,
-    ProductsListPageComponent
->>>>>>> 323e4fc41e370b3390badbe8a9adbdd402bdad8c
+    ProductsListPageComponent,
+    CarruselProductsComponent
   ],
   providers: [],
 })
