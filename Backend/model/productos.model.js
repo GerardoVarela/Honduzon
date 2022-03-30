a25c165b8f4f6cc389fc1503068f56f1cbdf9638
jorgevarela98 <jormivaroli98@hotmail.com>
2022-03-30 17:56:41 -0600

    feat: Detalle Producto (falta)

--- Backend/model/productos.model.js
+++ Backend/model/productos.model.js
@@ -228,7 +228,7 @@ async function getProductoPorId (productoId){
         var pool = await mssql.connect(bdConfig.config);
         let insertarProducto = await pool.request()
         .input('idProductoInput',mssql.VarChar,productoId)
-        .query('SELECT Productos.NOMBRE_PRODUCTO, Productos.DESCRIPCION_PRODUCTO, Productos.CANTIDAD_PRODUCTO, Productos.CANTIDAD_PROD_VENDIDO, Productos.PRECIO, IMAGENES.IMAGEN, Usuarios.NOMBRE, Usuarios.APELLIDO, DEPARTAMENTO.NOMBRE_DEPARTAMENTO,  Usuarios.IMAGENS FROM Productos JOIN IMAGENES ON Productos.ID_IMAGEN = IMAGENES.ID_IMAGEN JOIN Usuarios ON Productos.ID_USUARIO = Usuarios.ID_USUARIO JOIN DEPARTAMENTO ON Usuarios.ID_DEPARTAMENTO = DEPARTAMENTO.ID_DEPARTAMENTO WHERE Productos.ID_PRODUCTO = @idProductoInput')
+        .query('SELECT Productos.ID_CATEGORIA, Productos.NOMBRE_PRODUCTO, Productos.DESCRIPCION_PRODUCTO, Productos.CANTIDAD_PRODUCTO, Productos.CANTIDAD_PROD_VENDIDO, Productos.PRECIO, IMAGENES.IMAGEN, Usuarios.NOMBRE, Usuarios.APELLIDO, DEPARTAMENTO.NOMBRE_DEPARTAMENTO,  Usuarios.IMAGENS FROM Productos JOIN IMAGENES ON Productos.ID_IMAGEN = IMAGENES.ID_IMAGEN JOIN Usuarios ON Productos.ID_USUARIO = Usuarios.ID_USUARIO JOIN DEPARTAMENTO ON Usuarios.ID_DEPARTAMENTO = DEPARTAMENTO.ID_DEPARTAMENTO WHERE Productos.ID_PRODUCTO = @idProductoInput')
         return insertarProducto.recordset;
     } catch (error) {
         return error;
