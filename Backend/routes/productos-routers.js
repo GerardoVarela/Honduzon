7d07e8e24c585f252d20c9709dcd43ae7b4e1222
jorgevarela98 <jormivaroli98@hotmail.com>
2022-04-01 21:57:19 -0600

    readme.md Honduzon

--- Backend/routes/productos-routers.js
+++ Backend/routes/productos-routers.js
@@ -21,11 +21,9 @@ var numeroTotalProdUsusario = 0
 router.get('/obtenerdetalleproducto/:idProducto',(req,res)=>{
     productoModel.getProductoPorId(req.params.idProducto).then((resultado)=>{
         var productoPorId = {...resultado[0]}
-        console.log(productoPorId);
         productoModel.getCantTotalProdUsuario(resultado[0].ID_USUARIO).then(resultado=>{
             numeroTotalProdUsusario = resultado[0].CantTot;
             productoPorId['CantidadProdTotal'] = numeroTotalProdUsusario;
-            console.log(productoPorId);
             res.send(productoPorId);
             return;
         })
