c1f86c29b33653cf9f3c3a60006954762db09cfa
jorgevarela98 <jormivaroli98@hotmail.com>
2022-04-10 21:44:29 -0600

    agregado de sockets para chat, chat models y router... funca? no lo se :v el mundo esta lleno de sorpresas y estos archivos es una de ellas :vvvv

--- Backend/model/chat.model.js
+++ Backend/model/chat.model.js
@@ -20,9 +20,9 @@ async function nuevoChat (detalleChat){
     try {
         let pool = await mssql.connect(bdConfig.config);
         let nuevoChat = await pool.request()
-        .input('',mssql.Int,detalleChat.currentUser)
-        .input('',mssql.Int,detalleChat.usuario2)
-        .query('');
+        .input('idCurrentUser',mssql.Int,detalleChat.currentUser)
+        .input('idUsuario2',mssql.Int,detalleChat.usuario2)
+        .query('INSERT INTO CHAT(ID_USUARIO1, ID_USUARIO2) VALUES(@idCurrentUser,@idUsuario2)');
         return nuevoChat.recordset; 
     } catch (error) {
         return error;
@@ -37,8 +37,8 @@ async function getChatPorUsuario(idCurrentUser){
     try {
         let pool = await mssql.connect(bdConfig.config);
         let chatsDeUsuario = await pool.request()
-        .input('',mssql.Int,detalleChat.idCurrentUser)
-        .query('');
+        .input('idCurrentUser',mssql.Int,idCurrentUser)
+        .query('SELECT * FROM CHAT WHERE ID_USUARIO_EMISOR = @idCurrentUser');
         return chatsDeUsuario.recordset; 
     } catch (error) {
         return error;
@@ -51,18 +51,26 @@ si ell chat ya existe pues retornará los datos del chat
 en caso contrario, si no existe pues retornaria un dataset vacio
 
 */ 
-async function existenciaChat(){
+async function existenciaChatEntreUsuarios(currentUser, idUser2){
     try {
-        
+        try {
+            let pool = await mssql.connect(bdConfig.config);
+            let existenciaChatPorUsuario = await pool.request()
+            .input('currentUser',mssql.Int,currentUser)
+            .input('idUser2',mssql.Int,idUser2)
+            .query('SELECT * FROM CHAT WHERE ID_USUARIO1=currentUser AND ID_USUSARIO2 = @idUser2');
+            return existenciaChatPorUsuario.recordset; 
+        } catch (error) {
+            return error;
+        }
     } catch (error) {
         return error;
     }
 }
 
 
-
 module.exports={
     nuevoChat,
     getChatPorUsuario,
-    existenciaChat
+    existenciaChatEntreUsuarios,
 }
