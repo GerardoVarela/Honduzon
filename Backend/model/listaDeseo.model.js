e8cb75fbf3fb210d6d15a0156841816815ee6b44
jorgevarela98 <jormivaroli98@hotmail.com>
2022-04-27 22:10:03 -0600

    i dunno whatupp

---
+++ Backend/model/listaDeseo.model.js
@@ -0,0 +1,81 @@
+var bdConfig = require ('../config/bd-config');
+var mssql = require('mssql');
+
+
+
+async function agregarProductosListaDeseos(detalleListaDeseo){
+    try {
+        var pool = await mssql.connect(bdConfig.config);
+        let agregarProductosListaDeseo =  await pool.request()
+        .input('idProducto',mssql.Int,detalleListaDeseo.ID_PRODUCTO)
+        .input('idUsuario',mssql.Int,detalleListaDeseo.CURRENT_USER)
+        .query('INSERT INTO LISTA_DESEOS VALUES(@idProducto,@idUsuario,1)');
+        return agregarProductosListaDeseo.recordset;
+    } catch (error) {
+        return error;
+    }
+}
+
+
+async function getListaVerificacionDeseosUsuario(productoListaDeseo){
+    try {
+        var pool = await mssql.connect(bdConfig.config);
+        let obtenerListaDeseosUsuarios = await pool.request()
+        .input('idUsuario',mssql.Int,productoListaDeseo.ID_USUARIO)
+        .input('idProducto',mssql,productoListaDeseo.ID_PRODUCTO)
+        .query('SELECT * FROM LISTA_DESEO WHERE ID_PRODUCTO=@idProducto AND ID_USUARIO=@');
+        return obtenerListaDeseosUsuarios.recordset;
+    } catch (error) {
+        return error;
+    }
+}
+
+
+/**
+ * 
+ * Para obtener la lista de deseo por usuario
+ * 
+ */
+
+async function getListaDeseoUsuario(detalleUsuarioListaDeseo){
+    try {
+        var pool = await mssql.connect(bdConfig.config);
+        let obtenerListaDeseoUsuario = await pool.request()
+        .input('idUsuario',mssql,idUsuario)
+        .query('');
+        return obtenerListaDeseoUsuario.recordset
+    } catch (error) {
+        return error;
+    }
+}
+
+
+async function darBajaProductoListaDeseos(detalleListaDeseo){
+    try {
+        var pool = await mssql.connect(bdConfig.config);
+        let darBajaProductoListaDeseo =  await pool.request()
+        .input('idProducto',mssql.Int,detalleListaDeseo.ID_PRODUCTO)
+        .input('idUsuario',mssql.Int,detalleListaDeseo.CURRENT_USER)
+        .query('UPDATE LISTA_DESEOS SET ESTADO = 0 WHERE ID_USUARIO=@idUsuario AND ID_PRODUCTO= @idProducto');
+        return darBajaProductoListaDeseo.recordset;
+    } catch (error) {
+        return error;
+    }
+}
+
+
+async function borrarProductoListaDeseos(detalleListaDeseo){
+    try {
+        
+    } catch (error) {
+        return error;
+    }
+}
+
+
+module.exports={
+    agregarProductosListaDeseos,
+    darBajaProductoListaDeseos,
+    borrarProductoListaDeseos,
+    getListaVerificacionDeseosUsuario
+}
---
+++ Backend/routes/listaDeseos.router.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+var listaDeseosModel = require('../model/listaDeseo.model');
+
+router.post('/guardarArticulo',(req,res)=>{});
+
+
+router.delete('',(req,res)=>{
+
+});
+
+
+router.put('',(req,res)=>{
+
+});
+
+router.get('',(req,res)=>{
+
+});
+
+router.get('',(req,res)=>{
+    
+});
+
+module.exports={
+    router
+}
