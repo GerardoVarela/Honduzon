af9afa4a07d2a3f1120d3e2b71075591a22cd93f
jose dominguez <jaamadord@unah.hn>
2022-05-01 18:10:13 -0600

    agregados model y router metricas

---
+++ Backend/routes/metric.router.js
@@ -0,0 +1,37 @@
+const { json } = require('body-parser');
+var express = require('express');
+var router = express.Router();
+var metricasModel = require('../model/metricas-model');
+
+//obtener categorias mas suscritas
+router.get('/categoriasMasSuscritas',(req,res, next)=>{
+    metricasModel.categoriasMasSuscritas().then(resultado=>{
+        if(resultado.length==0){
+            res.send(0);}
+        else{
+        res.status(200).json(resultado);}
+    });
+}
+);
+
+//obtener categorias con mas productos
+router.get('/categoriasConMasProductos',(req,res, next)=>{
+    metricasModel.categoriasConMasProductos().then(resultado=>{
+        if(resultado.length==0){
+            res.send(0);}
+        else{
+        res.status(200).json(resultado);}
+    });
+}
+);
+
+//obtener usuarios mejor valorados
+router.get('/usuariosMejorValorados',(req,res, next)=>{
+    metricasModel.usuariosMejorValorados().then(resultado=>{
+        if(resultado.length==0){
+            res.send(0);}
+        else{
+        res.status(200).json(resultado);}
+    });
+}
+);
